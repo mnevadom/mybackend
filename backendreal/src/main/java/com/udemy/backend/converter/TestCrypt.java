2cd084c8cc3af3fffb46afa07cd6587909b5da5c
Mario Nevado <nevado.mario@gmail.com>
2021-02-10 09:20:01 +0100

    pruebas

--- backendreal/src/main/java/com/udemy/backend/converter/TestCrypt.java
+++ backendreal/src/main/java/com/udemy/backend/converter/TestCrypt.java
@@ -16,7 +16,7 @@ public class TestCrypt {
 	public static void main(String[] args) {
 		BCryptPasswordEncoder pe = new BCryptPasswordEncoder();
 		
-		System.out.println(pe.encode("user"));
+		System.out.println(pe.encode("Dogood01"));
 	}
 
 }
